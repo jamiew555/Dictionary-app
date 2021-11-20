90f007b5b17b7b5ea00c8bd0c1277f14889c9c5a
jamiew555 <89786834+jamiew555@users.noreply.github.com>
2021-11-20 13:31:12 +0000

    search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="Container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="footer">Coded by Fung-ping Wong</footer>
+      </div>
     </div>
   );
 }
-
-export default App;
